04e41c49045667c1fc93cf38943a3eb25dfe10c8
jyouma <juri19771211@gmail.com>
2025-01-22 16:41:03 +0900

    スキーマ定義を追加し、useFormを使用してフォームを作成

--- src/pages/specialty-form.tsx
+++ src/pages/specialty-form.tsx
@@ -1,10 +1,26 @@
 import { NextPage } from "next";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  postalCode: z
+    .string()
+    .min(1,{message: "郵便番号は必須です"})
+    .regex(/^\d{7}$/,{message: "郵便番号は7桁の数字を指定してください"}),
+  localSpecialty: z
+    .string()
+    .min(1,{message: "特産品名は必須です"}),
+});
+
+type FormSchema = z.infer<typeof schema>; 
 
 const SpecialtyForm: NextPage = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormSchema>({
+    resolver: zodResolver(schema),
+  });
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: FormSchema) => {
     console.log(data);
   };
 
@@ -12,7 +28,17 @@ const SpecialtyForm: NextPage = () => {
     <div>
       <h1>特産品投稿フォーム</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {/* フォーム要素を追加する */}
+      <div>
+          <label htmlFor="postalCode">郵便番号:</label>
+          <input id="postalCode" {...register("postalCode")} />
+          <p>{errors.postalCode?.message}</p>
+        </div>
+        <div>
+          <label htmlFor="localSpecialty">特産品:</label>
+          <input id="localSpecialty" {...register("localSpecialty")} />
+          <p>{errors.localSpecialty?.message}</p> 
+        </div>
+        <button type="submit">送信</button>
       </form>
     </div>
   );
